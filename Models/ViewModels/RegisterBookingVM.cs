84fad212c5c3cbb8853a652eb877a5e5d175810e
Qassamala <abdigobena@hotmail.com>
2020-03-30 10:43:34 +0200

    Add project files.

---
+++ Models/ViewModels/RegisterBookingVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Models.ViewModels
+{
+    public class RegisterBookingVM
+    {
+        public int BookingNumber { get; set; }
+        public string ClientSSN { get; set; }
+        public string CarType { get; set; }
+        public string CarLicenseNumber { get; set; }
+        public DateTime TimeOfPickUp { get; set; }
+        public int CurrentOdometer { get; set; }
+    }
+}
---
+++ Models/ViewModels/RegisterReturnVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Models.ViewModels
+{
+    public class RegisterReturnVM
+    {
+        public int BookingNumber { get; set; }
+        public DateTime TimeOfReturn { get; set; }
+        public int DistanceCovered { get; set; }
+    }
+}
