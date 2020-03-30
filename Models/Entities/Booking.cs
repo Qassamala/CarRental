41b0a76923ee63ff200a2458409ab599accc090d
Qassamala <abdigobena@hotmail.com>
2020-03-30 11:23:02 +0200

    Scaffolded CarRentalContext

---
+++ Models/Entities/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarRental.Models.Entities
+{
+    public partial class Booking
+    {
+        public int BookingNumber { get; set; }
+        public string ClientSsn { get; set; }
+        public string CarType { get; set; }
+        public string CarLicenseNumber { get; set; }
+        public DateTime? TimeOfBooking { get; set; }
+        public int? CurrentMileage { get; set; }
+    }
+}
---
+++ Models/Entities/CarRentalContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CarRental.Models.Entities
+{
+    public partial class CarRentalContext : DbContext
+    {
+        public CarRentalContext()
+        {
+        }
+
+        public CarRentalContext(DbContextOptions<CarRentalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Booking> Booking { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Booking>(entity =>
+            {
+                entity.HasKey(e => e.BookingNumber)
+                    .HasName("PK__Booking__AAC320BE51961604");
+
+                entity.Property(e => e.CarLicenseNumber)
+                    .HasMaxLength(7)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CarType)
+                    .HasMaxLength(32)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ClientSsn)
+                    .HasColumnName("ClientSSN")
+                    .HasMaxLength(32)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TimeOfBooking).HasColumnType("date");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
